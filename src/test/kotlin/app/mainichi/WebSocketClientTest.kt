18b5f25055bdf004364c37fc1c837dd3d1334b87
Amy <dragonhel5@gmail.com>
2020-11-26 22:27:45 +0100

    slightly better "test"

--- src/test/kotlin/app/mainichi/WebSocketClientTest.kt
+++ src/test/kotlin/app/mainichi/WebSocketClientTest.kt
@@ -1,35 +1,19 @@
 package app.mainichi
 
-import app.mainichi.objects.Post
+import app.mainichi.controller.WebSocketController
+import kotlinx.coroutines.reactive.collect
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.codec.cbor.Jackson2CborDecoder
-import org.springframework.http.codec.cbor.Jackson2CborEncoder
-import org.springframework.messaging.rsocket.RSocketRequester
-import org.springframework.messaging.rsocket.RSocketStrategies
-import org.springframework.messaging.rsocket.connectWebSocketAndAwait
-import org.springframework.messaging.rsocket.retrieveFlux
-import java.net.URI
 
 @SpringBootTest
-class WebSocketClientTest {
+class WebSocketClientTest(@Autowired val webSocketController: WebSocketController) {
     @Test
     fun testStream(): Unit = runBlocking {
-        RSocketRequester.builder()
-            .rsocketStrategies(
-                RSocketStrategies.builder()
-                    .encoders { it.add(Jackson2CborEncoder()) }
-                    .decoders { it.add(Jackson2CborDecoder()) }
-                    .build()
-            )
-            .connectWebSocketAndAwait(URI("ws://localhost:8181/gateway"))
-            .route("feed")
-            .retrieveFlux<Post>()
-            .map {
+        webSocketController.getFeed()
+            .collect {
                 println(it.content)
-                it
             }
-            .blockLast()
     }
 }
